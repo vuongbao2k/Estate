cc8e1dd164a2a55e642ef9173cc76ddd0a396f4c
giabao <giabao@MacBook-Pro-cua-giabao.local>
2024-07-24 16:14:53 +0700

    update JPA

--- src/main/java/com/javaweb/repository/impl/BuildingRepositoryImpl.java
+++ src/main/java/com/javaweb/repository/impl/BuildingRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.javaweb.repository.impl;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -19,18 +21,105 @@ public class BuildingRepositoryImpl implements BuildingRepositoty {
 
 	@PersistenceContext
 	private EntityManager entityManager;
+	
+	public static void joinTable(BuildingSearchBuilder buildingSearchBuilder, StringBuilder sql) {
+		Long staffId = buildingSearchBuilder.getStaffId();
+		if (staffId != null) {
+			sql.append(" INNER JOIN assignmentbuilding ab ON b.id = ab.buildingid ");
+		}
+		List<String> typeCode = buildingSearchBuilder.getTypeCode();
+		if (typeCode != null && typeCode.size() != 0) {
+			sql.append(" INNER JOIN buildingrenttype brt ON b.id = brt.buildingid ");
+			sql.append(" INNER JOIN renttype rt ON rt.id = brt.renttypeid ");
+		}
+	}
+
+	public static void queryNormal(BuildingSearchBuilder buildingSearchBuilder, StringBuilder where) {
+		try {
+			Field[] fields = BuildingSearchBuilder.class.getDeclaredFields();
+			for (Field item : fields) {
+				item.setAccessible(true);
+				String fieldName = item.getName();
+				if (!fieldName.equals("staffId") && !fieldName.equals("typeCode") && !fieldName.startsWith("rentArea")
+						&& !fieldName.startsWith("rentPrice")) {
+					Object value = item.get(buildingSearchBuilder);
+					if (value != null) {
+						if (item.getType().getName().equals("java.lang.Long")
+								|| item.getType().getName().equals("java.lang.Integer")) {
+							where.append(" AND b." + fieldName + " = " + value);
+						} else if (item.getType().getName().equals("java.lang.String")) {
+							where.append(" AND b." + fieldName + " LIKE '%" + value + "%' ");
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void querySpecial(BuildingSearchBuilder buildingSearchBuilder, StringBuilder where) {
+		Long staffId = buildingSearchBuilder.getStaffId();
+		if (staffId != null) {
+			where.append(" AND ab.staffid = " + staffId);
+		}
+		Long rentAreaFrom = buildingSearchBuilder.getRentAreaFrom();
+		Long rentAreaTo = buildingSearchBuilder.getRentAreaTo();
+		if (rentAreaFrom != null || rentAreaTo != null) {
+			where.append(" AND EXISTS (SELECT * FROM rentarea ra WHERE ra.buildingid = b.id ");
+			if (rentAreaFrom != null) {
+				where.append(" AND ra.value >=" + rentAreaFrom);
+			}
+			if (rentAreaTo != null) {
+				where.append(" AND ra.value <=" + rentAreaTo);
+			}
+			where.append(") ");
+		}
+
+		Long rentPriceFrom = buildingSearchBuilder.getRentPriceFrom();
+		Long rentPriceTo = buildingSearchBuilder.getRentPriceTo();
+		if (rentPriceFrom != null) {
+			where.append(" AND b.rentprice >=" + rentPriceFrom);
+		}
+		if (rentPriceTo != null) {
+			where.append(" AND b.rentprice <=" + rentPriceTo);
+		}
+		List<String> typeCode = buildingSearchBuilder.getTypeCode();
+		if (typeCode != null && typeCode.size() != 0) {
+			where.append(" AND (");
+			String sql = typeCode.stream().map(it -> "rt.code LIKE '%" + it + "%' ")
+					.collect(Collectors.joining(" OR "));
+			where.append(sql);
+			where.append(") ");
+		}
+
+	}
 
 	@Override
 	public List<BuildingEntity> findAll(BuildingSearchBuilder buildingSearchBuilder) {
-		// JPQL
-//		String sql = "FROM BuildingEntity b WHERE b.id = 1";
-//		Query query = entityManager.createQuery(sql, BuildingEntity.class);
-//		return query.getResultList();
-		
-		//SQL Native
-		String sql = "SELECT * FROM building b WHERE b.name like '%building%'";
-		Query query = entityManager.createNativeQuery(sql, BuildingEntity.class);
+		StringBuilder sql = new StringBuilder(
+				"SELECT b.id, b.name, b.districtid, b.street, b.ward, b.numberofbasement, b.floorarea, b.rentprice, "
+						+ "b.managername, b.managerphonenumber, b.servicefee, b.brokeragefee FROM building b ");
+		joinTable(buildingSearchBuilder, sql);
+		StringBuilder where = new StringBuilder(" WHERE 1=1 ");
+		queryNormal(buildingSearchBuilder, where);
+		querySpecial(buildingSearchBuilder, where);
+		where.append(" GROUP BY b.id ");
+		sql.append(where);
+		Query query = entityManager.createNativeQuery(sql.toString(), BuildingEntity.class);
 		return query.getResultList();
 	}
 
+//	@Override
+//	public List<BuildingEntity> findAll(BuildingSearchBuilder buildingSearchBuilder) {
+//		// JPQL
+////		String sql = "FROM BuildingEntity b WHERE b.id = 1";
+////		Query query = entityManager.createQuery(sql, BuildingEntity.class);
+////		return query.getResultList();
+//		
+//		//SQL Native
+//		String sql = "SELECT * FROM building b WHERE b.name like '%building%'";
+//		Query query = entityManager.createNativeQuery(sql, BuildingEntity.class);
+//		return query.getResultList();
+//	}
 }
